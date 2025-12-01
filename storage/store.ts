58d7a058c954ca508f18192b09dd0c0b6e383b90
Loic Leforestier <loicleforestier.pro@gmail.com>
2025-12-01 10:29:40 +0100

    added store and begin data model

--- storage/store.ts
+++ storage/store.ts
@@ -8,3 +8,30 @@ export const useStorage = create((set) => ({
   resetCurrentParameter: () => set({ currentParameter: "", isModalOpened: false }),
   setCurrentParameter: (newParameter: string) => set({ currentParameter: newParameter, isModalOpened: true }),
 }))
+
+export const useGameStorage = create((set) => ({
+  previousStep: "none",
+  currentStep: "intro",
+  companySatisfaction: 0, //between 0 and 1
+  characterSanity: 1,
+  playerState: [],
+  characterStates: [],
+  currentDay: 0,
+}))
+
+export const useDayStorage = create((set) => ({
+  dailyOrder: [],
+  currentOrder: 0,
+  currentEmotions: [
+    {joy: 0},
+    {confidence: 0},
+    {fear: 1.},
+    {surprise: .2},
+    {sadness: .2},
+    {disgust: .2},
+    {anger: .2},
+    {vigilance: .2},
+  ],
+}))
+//Current Order
+//Current Day
