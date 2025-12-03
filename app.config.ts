e9798a6d1a9b334868ded97804940e1577e38a00
Loic Leforestier <loicleforestier.pro@gmail.com>
2025-12-03 14:28:48 +0100

    setup expo build

---
+++ app.config.ts
@@ -0,0 +1,67 @@
+  import { ConfigContext, ExpoConfig } from 'expo/config';
+
+// const NAME = "groupe8";
+// const SCHEME = "groupe8";
+const IDENTIFIER = `fr.gobelins.groupe8`;
+const PROJECT_ID = "0559d8a6-63a1-4d17-9260-69465e4ec483";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const identifier = process.env.APP_VARIANT === "development" ? `${IDENTIFIER}.dev` : IDENTIFIER;
+
+  return {
+    ...config,
+    name: "please-make-me-better",
+    owner: "ecni2027",
+   
+    "slug": "please-make-me-better",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "pleasemakemebetter",
+    "userInterfaceStyle": "automatic",
+    "newArchEnabled": true,
+    "ios": {
+      "supportsTablet": true,
+      "bundleIdentifier": identifier,
+    },
+    "android": {
+      "package": identifier,
+      "adaptiveIcon": {
+        "backgroundColor": "#E6F4FE",
+        "foregroundImage": "./assets/images/android-icon-foreground.png",
+        "backgroundImage": "./assets/images/android-icon-background.png",
+        "monochromeImage": "./assets/images/android-icon-monochrome.png"
+      },
+      "edgeToEdgeEnabled": true,
+      "predictiveBackGestureEnabled": false
+    },
+    "web": {
+      "output": "single",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#ffffff",
+          "dark": {
+            "backgroundColor": "#000000"
+          }
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true,
+      "reactCompiler": true
+    },
+    "extra": {
+      "eas": {
+        projectId: PROJECT_ID,
+      }
+    }
+  }
+}
