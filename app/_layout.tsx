e26f211966903b0bc12b2622a9ded9940f926ba1
Emilien <emilien.ginko@gmail.com>
2025-12-03 14:08:02 +0100

    feat layout: set gpu canvas full size

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,21 @@
 // import GL from "@/ui/GL";
 import GPU from "@/ui/GPU";
 import { Stack } from "expo-router";
+import { View } from "react-native";
+
 export default function RootLayout() {
   return <>
+
+    <View
+      style={{
+        position: "fixed",
+        zIndex: 10,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <GPU />
+    </View>
     <Stack />
-    <GPU />
   </>;
 }
