9fe38c8f025de056e8f334467db7e19478372e7f
Emilien <emilien.ginko@gmail.com>
2025-12-08 17:47:52 +0100

    feat ui: border and gradient on ClientRequestList

--- ui/ClientRequestList/index.tsx
+++ ui/ClientRequestList/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-native";
 import React from "react";
 import {createStyle} from "./style";
-import ClientRequestTab from "@/ui/ClientRequestTab";
+import {LinearGradient} from 'expo-linear-gradient';
 import ClientRequestCard from "@/ui/ClientRequestCard";
 
 interface ClientRequestList extends ViewProps {
@@ -35,33 +35,43 @@ const ClientRequestList: React.FC<ClientRequestList> = (props) => {
 
     const Style = createStyle();
 
-    return <View style={Style.main} {...rest} >
+    return <View style={Style.outline}>
+        <LinearGradient
+            colors={["#6B6B6B", "rgba(153, 153, 153, 0)"]}
+            style={Style.gradient}
+            locations={[0, 1]} //Values to ajust when figma will be available
+            start={{x: 0, y: 1}} //Values to ajust when figma will be available
+            end={{x: 0, y: 0}} //Values to ajust when figma will be available
+        />
 
-        <Text style={Style.title}>{title}</Text>
+        <View style={Style.main} {...rest} >
 
-        <View style={Style.list} {...rest} >
-            {children && <>{children}</>}
+            <Text style={Style.title}>{title}</Text>
 
-            {
-                elements && <>
-                    {
-                        elements.map(element => {
-                            return (<React.Fragment key={element.id}>
-                                    <ClientRequestCard
-                                        selectColor={element.selectColor}
-                                        title={element.title}
-                                        paragraph={element.paragraph}
-                                        count={element.count}
-                                        onPress={() => selectRequest(element.id)}
-                                    />
-                                </React.Fragment>
-                            )
-                        })
-                    }
-                </>
-            }
+            <View style={Style.list} {...rest} >
+                {children && <>{children}</>}
+
+                {
+                    elements && <>
+                        {
+                            elements.map(element => {
+                                return (<React.Fragment key={element.id}>
+                                        <ClientRequestCard
+                                            selectColor={element.selectColor}
+                                            title={element.title}
+                                            paragraph={element.paragraph}
+                                            count={element.count}
+                                            onPress={() => selectRequest(element.id)}
+                                        />
+                                    </React.Fragment>
+                                )
+                            })
+                        }
+                    </>
+                }
 
 
+            </View>
         </View>
     </View>
 
--- ui/ClientRequestList/tokens.ts
+++ ui/ClientRequestList/tokens.ts
@@ -4,6 +4,7 @@ import {fontTokens} from "@/tokens/primary/font.tokens";
 export const COMPONENT_NAME = "client_request_list";
 
 export const clientRequestListTokens = {
+    [`${COMPONENT_NAME}-border-radius`]: 8,
 
     [`${COMPONENT_NAME}-main-text-primary`]: primaryColorTokens["color-white"],
     [`${COMPONENT_NAME}-main-background-color`]: primaryColorTokens["color-tertiary-high"],
