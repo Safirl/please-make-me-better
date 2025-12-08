97874c6842db4d4cd031e55a3e235fce24cdd1cf
Emilien <emilien.ginko@gmail.com>
2025-12-08 17:47:52 +0100

    feat ui: border and gradient on ClientRequestList

--- ui/ClientRequestList/style.ts
+++ ui/ClientRequestList/style.ts
@@ -9,18 +9,32 @@ import {
 } from "./tokens"
 
 const outOfFlowOffset = 10
-
 export const createStyle = () => StyleSheet.create({
     main: {
         gap: outOfFlowOffset + 24,
         backgroundColor: clientRequestListTokens[`${COMPONENT_NAME}-main-background-color`],
         padding: 16,
+        borderRadius: clientRequestListTokens[`${COMPONENT_NAME}-border-radius`],
     },
     title: {
         ...Fonts["h1"]
     },
     list: {
         gap: outOfFlowOffset + 12,
+        borderRadius: clientRequestListTokens[`${COMPONENT_NAME}-border-radius`],
+    },
+    outline: {
+        padding: 1,
+        borderRadius: clientRequestListTokens[`${COMPONENT_NAME}-border-radius`],
+    },
+    gradient: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        zIndex: -100,
+        borderRadius: clientRequestListTokens[`${COMPONENT_NAME}-border-radius`],
     }
 })
 
