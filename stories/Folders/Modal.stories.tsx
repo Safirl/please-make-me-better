f1378a4b15656bdf5eec14c616c6d0821f98f114
Emilien <emilien.ginko@gmail.com>
2025-12-11 18:03:04 +0100

    feat ui: create the folder section

--- stories/Folders/Modal.stories.tsx
+++ stories/Folders/Modal.stories.tsx
@@ -2,7 +2,7 @@ import Modal from '@/ui/Folders/index';
 import type { Meta, StoryObj } from '@storybook/react-native-web-vite';
 import { View } from "react-native";
 // import pp from "./assets/images/clients/niel_musk_pp.jpg";
-
+import { fn } from 'storybook/test';
 // console.log(pp)
 
 const meta = {
@@ -10,7 +10,7 @@ const meta = {
     component: Modal,
     decorators: [
         (Story) => (
-            <View style={{ flex: 1, alignItems: 'flex-start', paddingTop: 200}}>
+            <View style={{ flex: 1, alignItems: 'flex-start', paddingTop: 200 }}>
                 <Story />
             </View>
         ),
@@ -19,7 +19,7 @@ const meta = {
     tags: ['autodocs'],
     // Use `fn` to spy on the onPress arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
     args: {
-        // configure: fn()
+        configure: fn()
     },
     argTypes: {
         selectColor: {
@@ -43,7 +43,62 @@ export const Primary: Story = {
     args: {
         selectColor: "secondary",
         tabs: ["Dossier", "Lore"],
-        type: ""
+        type: "",
+
+        client: {
+            name: "NIEL MUSK",
+            request: "Je veux être perçu comme un leader",
+            work: "Conseillé en banque",
+            age: "28 ans",
+            profilePicture: `./assets/images/clients/niel_musk_pp.jpg`
+        },
+        gridContent: [
+            {
+                name: "Emotions",
+                labels: [{
+                    label: "Lorem Ipsum",
+                    status: "locked"
+                },
+                {
+                    label: "Lorem Ipsum",
+                    status: "neutral"
+                },
+                {
+                    label: "Lorem Ipsum",
+                    status: "neutral"
+                }]
+            },
+            {
+                name: "Emotions",
+                labels: [{
+                    label: "Lorem Ipsum",
+                    status: "locked"
+                },
+                {
+                    label: "Lorem Ipsum",
+                    status: "neutral"
+                },
+                {
+                    label: "Lorem Ipsum",
+                    status: "neutral"
+                }]
+            },
+            {
+                name: "Emotions",
+                labels: [{
+                    label: "Lorem Ipsum",
+                    status: "locked"
+                },
+                {
+                    label: "Lorem Ipsum",
+                    status: "neutral"
+                },
+                {
+                    label: "Lorem Ipsum",
+                    status: "neutral"
+                }]
+            },
+        ]
     },
 
 };
