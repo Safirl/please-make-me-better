aa30be508c4e4d26bd27115e367484508a76ead1
Emilien <emilien.ginko@gmail.com>
2025-12-11 17:15:12 +0100

    feat ui: create folders layout

--- ui/Folders/index.tsx
+++ ui/Folders/index.tsx
@@ -1,56 +1,149 @@
+import React from "react";
 import type {
     ViewProps,
 } from "react-native";
 import {
+    Pressable,
     Text,
     View
 } from "react-native";
-import Svg, { Path } from 'react-native-svg';
+import Svg, { Path } from "react-native-svg";
+import Button from "../Button";
 import { createStyle } from "./style";
-import { clientRequestTabTokens, COMPONENT_NAME } from "./tokens";
-
-interface CustomButtonProps extends ViewProps {
+import {
+    clientRequestTabTokens,
+    COMPONENT_NAME
+} from "./tokens";
+interface CustomModalProps extends ViewProps {
     selectColor?: "primary" | "secondary" | "tertiary";
-    title: string;
-    paragraph: string;
+    type: '' | ''
+    tabs: ('Dossier' | "Lore")[]
 }
 
 
-const ClientRequestTab: React.FC<CustomButtonProps> = (props) => {
+const Modal: React.FC<CustomModalProps> = (props) => {
 
     const {
         selectColor = "tertiary",
-        title,
-        paragraph,
+        tabs,
         ...rest
     } = props
 
     const Style = createStyle(selectColor);
 
+    const [selectedTab, setSelectedTab] = React.useState(tabs[0])
+    const [backButtonWidth, setBackButtonWidth] = React.useState()
+    const [backButtonHeight, setBackButtonHeight] = React.useState()
+
     return <View
-        style={Style.main}
+        style={{
+            width: "100%"
+        }}
         {...rest}
     >
+
+
+
         <View
-            style={Style.borderTop}
+            style={[Style.back, {
+                transform: [{ translateY: -(backButtonHeight || 0) - 5 }]
+            }]}
+            onLayout={(e) => {
+                if (!backButtonWidth) setBackButtonWidth(e.nativeEvent.layout.width);
+                if (!backButtonHeight) setBackButtonHeight(e.nativeEvent.layout.height);
+            }}
         >
-            <Svg width="169" height="19" viewBox="0 0 169 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <Path
-                    d="M1.03384 3.50479L0.532456 18.1936L168.799 11.4973V10.875L167.078 10.7105L151.714 0.679932L5.06496 1.02906L2.43036 1.21945L1.03384 3.50479Z"
-                    fill={clientRequestTabTokens[`${COMPONENT_NAME}-main-border-color-tertiary`]}/>
-                <Path
-                    d="M5 0V1H150.791V0H5ZM153.661 0.906113L153.087 1.72489L165.386 10.3482L165.96 9.52943L153.661 0.906113ZM165.96 9.52943L165.386 10.3482C166.395 11.0559 167.598 11.4355 168.83 11.4355V10.4355C168.008 10.4355 166.633 10.0012 165.96 9.52943ZM150.791 0V1C151.613 1 152.414 1.2531 153.087 1.72489L153.661 0.906113C152.651 0.198432 152.024 0 150.791 0ZM5 0C1.68629 0 0 1.68629 0 5H1C1 2.79086 2.79086 1 5 1V0Z"
-                    fill={clientRequestTabTokens[`${COMPONENT_NAME}-main-border-color-${selectColor}`]}/>
-                <Path d="M1 5H0V18.4355H1V5Z"
-                      fill={clientRequestTabTokens[`${COMPONENT_NAME}-main-border-color-${selectColor}`]}/>
-            </Svg>
-
-
+            <Button
+                type="back"
+                label={"< retour"}
+            />
         </View>
 
-        {title && <Text style={Style.titleFont}>{title} // </Text>}
-        {paragraph && <Text style={Style.paragraphFont}>{paragraph}</Text>}
+        {
+            tabs.map((tab, i) => {
+                return <Tab
+                    label={tab}
+                    index={i}
+                    Style={Style}
+                    selected={selectedTab === tab}
+                    setSelectedTab={setSelectedTab}
+                    offset={backButtonWidth || 0}
+                />
+            })
+        }
 
-    </View>
+
+        <View
+            style={Style.main}
+        >
+        </View>
+
+    </View >
 }
-export default ClientRequestTab
+export default Modal
+
+
+
+
+const Tab = ({ Style, label, index, selected, offset, setSelectedTab }: { Style: any, label: string, index: number, selected: boolean, offset: number, setSelectedTab: (v: string) => string }) => {
+
+
+    const [labelWidth, setLabelWidth] = React.useState(null);
+    const [buttonWidth, setButtonWidth] = React.useState(null);
+    const [buttonHeight, setButtonHeight] = React.useState(null);
+
+    return <Pressable
+        onLayout={(e) => {
+            if (!buttonWidth) setButtonWidth(e.nativeEvent.layout.width);
+            if (!buttonHeight) setButtonHeight(e.nativeEvent.layout.height);
+        }}
+        onPress={() => setSelectedTab(label)}
+        style={[{
+            zIndex: selected ? clientRequestTabTokens[`${COMPONENT_NAME}-tab-selected-index`] : clientRequestTabTokens[`${COMPONENT_NAME}-tab-not-selected-index`]
+        }, {
+            position: "absolute" as const,
+
+            top: 0,
+            left: labelWidth ? offset + index * buttonWidth - (index + 1) * 3 : 0,
+            transform: [{ translateY: - (buttonHeight || 0) + 1 }],
+            flexDirection: "row",
+        }]}
+    >
+
+        <Text
+            onLayout={(e) => {
+                if (!labelWidth) setLabelWidth(e.nativeEvent.layout.width);
+            }}
+            style={
+                [Style.btnLabel,
+                labelWidth && {
+                    transform: [{ translateX: -labelWidth / 2 - index * 8 }, { translateY: "-50%" }]
+                }
+                ]
+            }
+
+        >
+            {label}
+        </Text>
+
+
+        <Svg
+            style={Style.svgs}
+            transform={[{ translateX: -index * 8 }, { translateY: 2.5 + 2.84 }]}
+            width="175"
+            height="41"
+            viewBox="0 0 175 41"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <Path d="M1.13379 35.5358L0 40.3739L174.863 38.8802V35.4773L172.417 35.077L169.725 33.3867L168.436 31.4962L159.961 5.66034L159.069 3.52853L157.375 1.63863L155.455 0.671006L153.822 0.383728H40.8214H15.7085L13.0778 0.655871L11.2181 1.82005L9.23748 4.45079L8.60248 6.24996V22.6694V26.177L8.39081 29.1858L7.4383 31.9526L4.97387 34.4019L2.64552 35.3393L1.13379 35.5358Z"
+                fill={
+                    selected
+                        ? clientRequestTabTokens[`${COMPONENT_NAME}-tab-selected-color`]
+                        : clientRequestTabTokens[`${COMPONENT_NAME}-tab-not-selected-color`]
+                }
+            />
+            <Path d="M174.938 35.4C171.736 35.3999 168.897 33.339 167.903 30.2945L159.635 4.95272C158.749 2.23735 156.217 0.400039 153.36 0.399994H15.1221C11.477 0.399994 8.52247 3.35452 8.52247 6.9996V27.9996C8.52247 32.0865 5.20897 35.4 1.12207 35.4" stroke="#6FFFB0" stroke-width="0.8" />
+        </Svg>
+    </Pressable>
+}
