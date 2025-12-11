5d1edf041335c0a9863cb8cb4e3a52bc1dbd35c1
Emilien <emilien.ginko@gmail.com>
2025-12-11 17:15:12 +0100

    feat ui: create folders layout

---
+++ stories/Folders/Modal.stories.tsx
@@ -0,0 +1,50 @@
+import Modal from '@/ui/Folders/index';
+import type { Meta, StoryObj } from '@storybook/react-native-web-vite';
+import { View } from "react-native";
+// import pp from "./assets/images/clients/niel_musk_pp.jpg";
+
+// console.log(pp)
+
+const meta = {
+    title: 'UI/Folder/Modal',
+    component: Modal,
+    decorators: [
+        (Story) => (
+            <View style={{ flex: 1, alignItems: 'flex-start', paddingTop: 200}}>
+                <Story />
+            </View>
+        ),
+    ],
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+    // Use `fn` to spy on the onPress arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
+    args: {
+        // configure: fn()
+    },
+    argTypes: {
+        selectColor: {
+            options: ["primary", "secondary", "tertiary", undefined],
+            control: { type: "radio" },
+        },
+    },
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/design/5VGPGRjnfdWU60rwAdXXPe/Maquette-App-Configurateur?node-id=526-1641&t=WELEg4XJp0E2GD5j-1'
+        }
+    }
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        selectColor: "secondary",
+        tabs: ["Dossier", "Lore"],
+        type: ""
+    },
+
+};
+
--- ui/Folders/style.ts
+++ ui/Folders/style.ts
@@ -1,12 +1,11 @@
-import {StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 
-import {primaryColorTokens} from "@/tokens/primary/colors.tokens";
-import {primaryFontTokens} from "@/tokens/primary/font.tokens";
-import Fonts from "@/assets/styles/fonts"
+import Fonts from "@/assets/styles/fonts";
+import { primaryFontTokens } from "@/tokens/primary/font.tokens";
 import {
     COMPONENT_NAME,
     clientRequestTabTokens,
-} from "./tokens"
+} from "./tokens";
 
 const titleFont = {
     ...Fonts.subTitle,
@@ -17,30 +16,50 @@ const paragraphFont = {
     color: primaryFontTokens["font-color-secondary"]
 }
 const main = (selectColor: "primary" | "secondary" | "tertiary" = "tertiary") => ({
-    gap: 12,
+
+    width: "100%" as const,
+    backgroundColor: clientRequestTabTokens[`${COMPONENT_NAME}-main-background-color`],
+
+    paddingTop: 32,
+    paddingRight: 12,
+    paddingLeft: 12,
+    paddingBottom: 12,
+
     borderWidth: 1,
-    paddingTop: 8,
-    paddingRight: 32,
-    paddingLeft: 32,
-    paddingBottom: 16,
     borderRadius: 5,
     borderColor: clientRequestTabTokens[`${COMPONENT_NAME}-main-border-color-${selectColor}`],
-    backgroundColor: clientRequestTabTokens[`${COMPONENT_NAME}-main-background-color`]
+    zIndex: 0
 })
+
 
 const borderTop = {
     position: "absolute" as const,
     flex: 1,
     top: 0,
     left: 0,
-    transform: [{translateY: -11.5}, {translateX: -1}],
+    transform: [{ translateY: -11.5 }, { translateX: -1 }],
 }
 
 export const createStyle = (selectColor: "primary" | "secondary" | "tertiary" = "tertiary") => StyleSheet.create({
     main: main(selectColor),
-    titleFont,
-    paragraphFont,
-    borderTop
+
+    back: {
+        position: "absolute" as const,
+        top: 0,
+
+
+    },
+    btnLabel: {
+        ...Fonts.paragraph,
+
+        position: "absolute" as const,
+
+        top: "50%",
+        left: "50%",
+        zIndex: 1
+        // transform: [{ translateX: "-50%" }, { translateY: "-50%" }]
+
+    }
 })
 
 
