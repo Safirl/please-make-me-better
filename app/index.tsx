7a97f0eaa97f6f0c9ad830a1117f19cdb11128bc
Loic Leforestier <loicleforestier.pro@gmail.com>
2025-12-12 12:08:34 +0100

    added buttons on main page

--- app/index.tsx
+++ app/index.tsx
@@ -4,15 +4,21 @@ import { WordParameter } from "@/components/Parameters/wordParameter";
 import { Text } from "@react-navigation/elements";
 import { Link, router } from "expo-router";
 import { useEffect } from "react";
-import { Pressable, View } from "react-native";
+import { Dimensions, Pressable, View } from "react-native";
 import { useStorage } from "../storage/store";
 import Button from "@/ui/Button";
 import Helpers from "./utils/Helpers";
+import Svg, { Circle } from "react-native-svg";
+import { StyleSheet } from "react-native";
+
+
+const dimensions = Dimensions.get("window")
 
 export default function Index() {
   const currentParameter: string = useStorage((state: any) => state.currentParameter)
   const setCurrentParameter = useStorage((state: any) => state.setCurrentParameter)
 
+  console.log("dimensions", dimensions.width)
   useEffect(() => {
     if (!Helpers.isDevMode) return;
     let root = Helpers.instance.getGUIFolder();
@@ -29,26 +35,56 @@ export default function Index() {
         overflow: "hidden"
       }}
     >
-      <Modal>
-        {
-          currentParameter === "cursors" && <CursorParameter/> ||
-          // currentParameter === "wheel" && <WheelParameter/> ||
-          currentParameter === "words" && <WordParameter/>
-        }
-      </Modal>
-      <Pressable onPress={() => {setCurrentParameter("cursors")}}>
-        <Text>Open cursors</Text>
-      </Pressable>
-      <Button type="tertiary" label="Open memories" icon={{name: "personality"}} onPress={() => router.navigate("/memoriesPage")}>
-        {/* <Link href={"/memoriesPage"}>Open memories</Link> */}
-      </Button>
-      {/* </Pressable> */}
-      {/* <Pressable onPress={() => {setCurrentParameter("wheel")}}>
-        <Text>Open wheel</Text>
-      </Pressable> */}
-      <Pressable onPress={() => {setCurrentParameter("words")}}>
-        <Text>Open words</Text>
-      </Pressable>
+      <View style={styles.button1}>
+        <Button type="tertiary" label="Memories" icon={{name: "memory"}} onPress={() => router.navigate("/memoriesPage")}/>
+      </View>
+
+      <View style={styles.button2}>
+        <Button type="tertiary" label="Emotions" icon={{name: "emotion"}} onPress={() => router.navigate("/memoriesPage")}/>
+      </View> 
+
+      <View style={styles.button3}>
+        <Button type="tertiary" label="Personality" icon={{name: "personality"}} onPress={() => router.navigate("/memoriesPage")}/>
+      </View>     
+      
+      <Svg
+        style={styles.circle}
+        width={490}
+        height={394}
+        fill="none"
+      >
+        <Circle
+          cx={245}
+          cy={197}
+          r={(dimensions.width/2)*.7}
+          stroke="#969696"
+          strokeDasharray="11 11"
+        />
+      </Svg>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  circle: {
+    position: "absolute",
+    zIndex: -1
+  },
+
+  button1: {
+    position: "absolute",
+    left: "10%",
+    top: 20
+  },
+
+  button2: {
+    position: "absolute",
+    left: "10%",
+    top: 300
+  },
+
+  button3: {
+    position: "absolute",
+    right: "10%",
+  }
+})
--- ui/Button/index.tsx
+++ ui/Button/index.tsx
@@ -1,94 +1,100 @@
+import Font from "@/assets/styles/fonts";
 import { primaryColorTokens } from "@/tokens/primary/colors.tokens";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useState } from "react";
 import {
     Pressable,
-    PressableProps, StyleSheet,
+    PressableProps, StyleProp, StyleSheet,
     Text,
-    View
+    View,
+    ViewStyle,
 } from "react-native";
 import SvgComponent, { SvgComponentProps } from "../svg";
 
+//ommit styles
 interface CustomButtonProps extends PressableProps {
     type: 'primary' | 'secondary' | 'tertiary' | 'back';
     state?: 'none' | 'disabled' | 'active'
     label?: string;
     children?: React.ReactNode;
     icon?: SvgComponentProps,
+    style?: StyleProp<ViewStyle>
 }
 
-const styles = StyleSheet.create({
-    layout: {
-        position: "relative",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        gap: 8,
-        flexDirection: "row",
-        borderRadius: 4,
-    },
+const createStyles = (type: 'primary' | 'secondary' | 'tertiary' | 'back') => {
+    return StyleSheet.create({
+        layout: {
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingVertical: type === "back" ? 8 : 12,
+            paddingHorizontal: type === "back" ? 16 : 32,
+            gap: 8,
+            flexDirection: "row",
+            borderRadius: 4,
+        },
 
-    border: {
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        borderRadius: 5,
-    },
-    
-    content: {
-        color: primaryColorTokens['color-white'],
-        userSelect: "none",
-    },
+        border: {
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            borderRadius: 5,
+        },
 
-    contentSecondary: {
-        color: primaryColorTokens['color-secondary-medium'],
-        userSelect: "none",
-    },
+        content: {
+            userSelect: "none",
+            ...Font.paragraph
+        },
 
-    contentDisabled: {
-        color: primaryColorTokens['color-tertiary-lower'],
-    },
+        contentSecondary: {
+            color: primaryColorTokens['color-secondary-medium'],
+            userSelect: "none",
+        },
 
-    pressable:{
-        position: "relative",
-        padding: 1,
-        borderRadius: 5,
-    },
+        contentDisabled: {
+            color: primaryColorTokens['color-tertiary-lower'],
+        },
 
-    //Style modifiers
+        pressable: {
+            position: "relative",
+            padding: 1,
+            borderRadius: 5,
+        },
 
-    primary: {
-        backgroundColor: primaryColorTokens["color-primary-medium"],
-    },
+        //Style modifiers
 
-    primaryPressed: {
-        backgroundColor: primaryColorTokens["color-primary-high"],
-    },
+        primary: {
+            backgroundColor: primaryColorTokens["color-primary-medium"],
+        },
 
-    primaryDisabled: {
-        backgroundColor: primaryColorTokens["color-tertiary-low"],
-    },
+        primaryPressed: {
+            backgroundColor: primaryColorTokens["color-primary-high"],
+        },
 
-    secondary: {
-        backgroundColor: primaryColorTokens["color-tertiary-low"],
-    },
+        primaryDisabled: {
+            backgroundColor: primaryColorTokens["color-tertiary-low"],
+        },
 
-    secondaryPressed: {
-        backgroundColor: primaryColorTokens["color-tertiary-medium"],
-    },
+        secondary: {
+            backgroundColor: primaryColorTokens["color-tertiary-low"],
+        },
 
-    secondaryDisabled: {
-        backgroundColor: primaryColorTokens["color-tertiary-medium"],
-    },
+        secondaryPressed: {
+            backgroundColor: primaryColorTokens["color-tertiary-medium"],
+        },
 
-    tertiary: {
-        backgroundColor: primaryColorTokens["color-tertiary-medium"],
-    },
-})
+        secondaryDisabled: {
+            backgroundColor: primaryColorTokens["color-tertiary-medium"],
+        },
+
+        tertiary: {
+            backgroundColor: primaryColorTokens["color-tertiary-medium"],
+        },
+    })
+}
 
 const Button: React.FC<CustomButtonProps> = (props) => {
     const [isPressed, setIsPressed] = useState(false)
@@ -99,91 +105,99 @@ const Button: React.FC<CustomButtonProps> = (props) => {
         icon: iconName,
         children,
         label,
+        style,
         ...rest
     } = props
 
+    const styles = createStyles(type)
+
     const iconColor = state === "disabled"
-    ? styles.contentDisabled.color : state === "active" ? 
-    styles.contentSecondary.color : styles.content.color
+        ? styles.contentDisabled.color : state === "active" ?
+            styles.contentSecondary.color : styles.content.color
 
     const borderGradient = type === "back"
-    ? primaryColorTokens["gradient-border-2"] : type === "tertiary" && state === "active"
-    ? primaryColorTokens["gradient-border-3"] : type === "tertiary" ? primaryColorTokens["gradient-border-4"] : primaryColorTokens["gradient-border-1"]
+        ? primaryColorTokens["gradient-border-2"] : type === "tertiary" && state === "active"
+            ? primaryColorTokens["gradient-border-3"] : type === "tertiary" ? primaryColorTokens["gradient-border-4"] : primaryColorTokens["gradient-border-1"]
 
-    console.log("border ", borderGradient)
-    
+
     return <Pressable
-    onPressIn={() => {setIsPressed(true)}}
-    onPressOut={() => {setIsPressed(false)}}
-    style={[
-        styles.pressable, 
-        type === "primary" && 
-        [
-            styles.primary,
-            isPressed && styles.primaryPressed,
-            state == "disabled" && styles.primaryDisabled
-        ],
-        type === "secondary" && [
-            styles.secondary,
-            isPressed && styles.secondaryPressed,
-            state == "disabled" && styles.secondaryDisabled
-        ],
-        type === "back" && [
-            styles.secondary,
-            isPressed && styles.secondaryPressed,
-            state == "disabled" && styles.secondaryDisabled
-        ],
-        type === "tertiary" && [
-            styles.tertiary,
-        ]
-    ]}
-    {...rest}
+        onPressIn={() => {
+            setIsPressed(true)
+        }}
+        onPressOut={() => {
+            setIsPressed(false)
+        }}
+        style={[
+            styles.pressable,
+            type === "primary" &&
+            [
+                styles.primary,
+                isPressed && styles.primaryPressed,
+                state == "disabled" && styles.primaryDisabled
+            ],
+            type === "secondary" && [
+                styles.secondary,
+                isPressed && styles.secondaryPressed,
+                state == "disabled" && styles.secondaryDisabled
+            ],
+            type === "back" && [
+                styles.secondary,
+                isPressed && styles.secondaryPressed,
+                state == "disabled" && styles.secondaryDisabled
+            ],
+            type === "tertiary" && [
+                styles.tertiary,
+            ],
+            style
+        ]}
+        {...rest}
     >
-            <LinearGradient 
-                colors={borderGradient}
-                style={styles.border}
-            />
-            <View
-                style={[
-                    styles.layout,
-                    type === "primary" && [
-                        styles.primary,
-                        isPressed && styles.primaryPressed,
-                        state == "disabled" && styles.primaryDisabled
-                    ],
-                    type === "secondary" && [
-                        styles.secondary,
-                        isPressed && styles.secondaryPressed,
-                        state == "disabled" && styles.secondaryDisabled
-                    ],
-                    type === "back" && [
-                        styles.secondary,
-                        isPressed && styles.secondaryPressed,
-                        state == "disabled" && styles.secondaryDisabled
-                    ],
-                    type === "tertiary" && [
-                        styles.tertiary,
-                    ],
-                ]}
-            >
-                {
-                    iconName && <SvgComponent 
-                        color={iconColor}
-                        name={iconName.name}/>
-                }
-                {
-                    label
-                        ? <Text style={[
-                                styles.content,
-                                state == "disabled" && styles.contentDisabled,
-                                state == "active" && styles.contentSecondary,
-                            ]}
-                        >{label}</Text>
-                        : children
-                }
-            </View>
+        <LinearGradient
+            colors={borderGradient}
+            style={styles.border}
+        />
+        <View
+            style={[
+                styles.layout,
+                type === "primary" && [
+                    styles.primary,
+                    isPressed && styles.primaryPressed,
+                    state == "disabled" && styles.primaryDisabled
+                ],
+                type === "secondary" && [
+                    styles.secondary,
+                    isPressed && styles.secondaryPressed,
+                    state == "disabled" && styles.secondaryDisabled
+                ],
+                type === "back" && [
+                    styles.secondary,
+                    isPressed && styles.secondaryPressed,
+                    state == "disabled" && styles.secondaryDisabled
+                ],
+                type === "tertiary" && [
+                    styles.tertiary,
+                ],
+            ]}
+        >
+            {
+                iconName && <SvgComponent
+                    color={iconColor}
+                    name={iconName.name} />
+            }
+            {
+                label
+                    ? <Text style={[
+                        styles.content,
+                        state == "disabled" && styles.contentDisabled,
+                        state == "active" && styles.contentSecondary,
+                    ]}
+                    >{label}</Text>
+                    : children
+            }
+        </View>
     </Pressable>
 }
 
-export default Button;
+export default Button
+export { Button };
 
