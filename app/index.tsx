614c034c125e537cabaafec0ee42939607fccb47
Emilien <emilien.ginko@gmail.com>
2025-12-29 12:00:25 +0100

    ev-experiment-outside-animation

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { CursorParameter } from "@/ui/Parameters/cursorParameter";
 import { WordParameter } from "@/ui/Parameters/wordParameter";
 import { Text } from "@react-navigation/elements";
 import { Link, router } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Dimensions, Pressable, View } from "react-native";
 import { useStorage } from "../storage/store";
 import Button from "@/ui/Button";
@@ -10,7 +10,7 @@ import Helpers from "./utils/Helpers";
 import Svg, { Circle } from "react-native-svg";
 import { StyleSheet } from "react-native";
 import { Href } from "expo-router";
-
+import { Animated, Easing } from "react-native";
 const dimensions = Dimensions.get("window")
 
 export default function Index() {
@@ -25,14 +25,63 @@ export default function Index() {
     // Helpers.instance.tweak()
   }, [])
 
-  const buttons = [
-    { label: "Memories", icon: "memory" as const, route: "/memoriesPage" as Href, style: styles.button1 },
-    { label: "Emotions", icon: "emotion" as const, route: "/memoriesPage" as Href, style: styles.button2 },
-    { label: "Personality", icon: "personality" as const, route: "/memoriesPage" as Href, style: styles.button3 },
-  ];
+  const [buttons, setButtons] = useState([
+    {
+      label: "Memories",
+      icon: "memory" as const,
+      route: "/memoriesPage" as Href,
+      style: styles.button1,
+      tx: new Animated.Value(0),
+      animation: {
+        finalePosition: -350,
+        velocity: 1.15
+      }
+
+    },
+    {
+      label: "Emotions",
+      icon: "emotion" as const,
+      route: "/memoriesPage" as Href,
+      style: styles.button2,
+      tx: new Animated.Value(0),
+      animation: {
+        finalePosition: -350,
+        velocity: 1.3
+      }
+
+    },
+    {
+      label: "Personality",
+      icon: "personality" as const,
+      route: "/memoriesPage" as Href,
+      style: styles.button3,
+      tx: new Animated.Value(0),
+      animation: {
+        finalePosition: -650,
+        velocity: 1.8
+      }
+
+    },
+  ]);
 
   const handleNavigate = (route: Href) => {
-    router.navigate(route)
+    const animationDurationInMs = 660
+
+    buttons.map((button, i) => {
+      Animated.timing(button.tx, {
+        toValue: button.animation.finalePosition, /* We can replace the velocity factor by the distance to get out the screen. */
+        duration: animationDurationInMs,
+        delay: i * 60,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.cubic),
+      }).start()
+    })
+
+    setTimeout(() => {
+
+      router.navigate(route)
+
+    }, animationDurationInMs)
   }
 
   return (
@@ -47,15 +96,26 @@ export default function Index() {
 
 
       {
-        buttons.map(({ label, icon, route, style }) => (
-          <View key={label} style={style}>
+        buttons.map(({ label, icon, route, style, tx }) => (
+          <Animated.View
+            key={label}
+            style={[
+              style,
+              {
+                transform: [{ translateX: tx }],
+              }
+            ]
+            }
+
+
+          >
             <Button
               type="tertiary"
               label={label}
               icon={{ name: icon }}
               onPress={() => handleNavigate(route)}
             />
-          </View>
+          </Animated.View>
         ))
       }
 
@@ -88,16 +148,16 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: "10%",
     top: 20
-  },
+  } as any,
 
   button2: {
     position: "absolute",
     left: "10%",
     top: 300
-  },
+  } as any,
 
   button3: {
     position: "absolute",
     right: "10%",
-  }
+  } as any
 })
