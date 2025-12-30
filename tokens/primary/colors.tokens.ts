c2798cd1eed674b5fca75d33e3994e9ce0769bd1
Emilien <emilien.ginko@gmail.com>
2025-12-30 16:38:15 +0100

    feat button: create folder button

--- tokens/primary/colors.tokens.ts
+++ tokens/primary/colors.tokens.ts
@@ -3,24 +3,24 @@ import { ColorValue } from "react-native";
 export const primaryColorTokens = {
     "color-primary-high": "#684CF4" as ColorValue,
     "color-primary-medium": "#8770F9" as ColorValue,
-    "color-primary-low": "#A897FB"as ColorValue,
+    "color-primary-low": "#A897FB" as ColorValue,
 
-    "color-secondary-high": "#15FC80"as ColorValue,
-    "color-secondary-medium": "#6FFFB2"as ColorValue,
-    "color-secondary-low": "#DEFFED"as ColorValue,
+    "color-secondary-high": "#15FC80" as ColorValue,
+    "color-secondary-medium": "#6FFFB2" as ColorValue,
+    "color-secondary-low": "#DEFFED" as ColorValue,
 
-    "color-tertiary-high": "#050507"as ColorValue,
-    "color-tertiary-medium": "#1D1E22"as ColorValue,
+    "color-tertiary-high": "#050507" as ColorValue,
+    "color-tertiary-medium": "#1D1E22" as ColorValue,
     "color-tertiary-low": "#39393D" as ColorValue,
     "color-tertiary-low-transparent": "rgba(57, 57, 61, 0.2)" as ColorValue,
-    "color-tertiary-lower": "#969696"as ColorValue,
-    "color-tertiary-lowest": "#BFBFBF"as ColorValue,
+    "color-tertiary-lower": "#969696" as ColorValue,
+    "color-tertiary-lowest": "#BFBFBF" as ColorValue,
 
-    "color-danger-high": "#EB5557"as ColorValue,
+    "color-danger-high": "#EB5557" as ColorValue,
 
-    "color-warning-high": "#FFA45B"as ColorValue,
+    "color-warning-high": "#FFA45B" as ColorValue,
 
-    "color-white": "#F1F1F1"as ColorValue,
+    "color-white": "#F1F1F1" as ColorValue,
 
 
 
@@ -34,4 +34,5 @@ export const primaryColorTokens = {
     "gradient-border-2": ['rgba(134, 112, 249, 1)', 'rgba(134, 112, 249, 0.25)'] as [ColorValue, ColorValue],
     "gradient-border-3": ["#6FFFB2", "#6FFFB2"] as [ColorValue, ColorValue],
     "gradient-border-4": ["#F1F1F1", "#F1F1F1"] as [ColorValue, ColorValue],
+    "primart-accent-border": ["#8770F9", "#8770F9"] as [ColorValue, ColorValue],
 }
