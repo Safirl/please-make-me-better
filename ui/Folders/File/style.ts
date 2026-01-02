7d80e091b06e653956657ee8f18b8767ed9fb525
Emilien <emilien.ginko@gmail.com>
2026-01-02 17:46:43 +0100

    feat ui: create file template

---
+++ ui/Folders/File/style.ts
@@ -0,0 +1,64 @@
+import { StyleSheet } from "react-native";
+
+import Fonts from "@/assets/styles/fonts";
+import { primaryTokens } from "@/tokens/primary/primary.token";
+
+
+
+export const createStyle = () => StyleSheet.create({
+    main: {
+        flexDirection: "row",
+        gap: 32,
+        alignItems: "stretch" as const,
+    },
+    leftSection: {
+        paddingTop: 16,
+        alignItems: "center"
+    },
+    textArea: {
+        flex: 1
+    },
+    image: {
+        width: 112,
+        height: 209
+    },
+    title: {
+        ...Fonts["h2"],
+        paddingVertical: 10
+    },
+    hr:{
+        backgroundColor: primaryTokens["color-tertiary-low"],
+        margin: 0,
+        width: 375,
+        height: 1,
+        marginBottom: 12
+    },
+    subTitle: {
+        paddingBottom: 12,
+        paddingTop: 32,
+    },
+    sectionName: {
+        ...Fonts.paragraph,
+        paddingVertical: 8,
+    },
+    paragraph: {
+        ...Fonts.paragraph,
+        color: primaryTokens["color-tertiary-lower"],
+        paddingVertical: 4,
+    },
+    accentParagraph: {
+        color: primaryTokens["color-secondary-high"],
+        paddingVertical: 4,
+
+    },
+    marker: {
+        position: "absolute",
+        top: 21,
+        left: -50
+    }
+})
+
+
+
+
+
